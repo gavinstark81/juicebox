feaf5958e616b8c138ccb9ab06d0022247985cdc
1hazardousboi <sarinjthorn@gmail.com>
2022-07-13 10:37:59 -0600

    many changes, nearly finished with testing, wrap up (part 1)

--- db/index.js
+++ db/index.js
@@ -12,8 +12,27 @@ async function getAllUsers() {
   return rows;
 }
 
-// exporting helper function
+// creating users function
+async function createUser({ username, password }) {
+  try {
+    const result = await client.query(
+      `
+      INSERT INTO users(username, password)
+      VALUES ($1, $2);
+    `,
+      [username, password]
+    );
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// module.exports = { createUser };
+// exporting helper functions
 module.exports = {
   client,
   getAllUsers,
+  createUser,
 };
